9e7d32ab2502b3ad86200a8e0d9c929548407fee
sknsec <subhash.nsec@gmail.com>
2021-01-25 20:35:13 +0530

    1st commit

---
+++ src/test/java/com/subh/consul/api/SpringJenkinsApplicationTests.java
@@ -0,0 +1,23 @@
+package com.subh.consul.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	@Test
+	void contextLoads() {
+		
+		logger.info("Test case executed..");
+		assertEquals(true, true);
+		
+	}
+
+}
